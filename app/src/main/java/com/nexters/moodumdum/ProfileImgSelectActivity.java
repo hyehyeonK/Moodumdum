1346fbcc7e642350192d8fbe4001caa0632a9029
aa9390 <aa9390>
2018-03-08 01:20:04 +0900

    프로필 이미지 레이아웃 추가

---
+++ app/src/main/java/com/nexters/moodumdum/ProfileImgSelectActivity.java
@@ -0,0 +1,13 @@
+package com.nexters.moodumdum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileImgSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_profile_img_select );
+    }
+}
